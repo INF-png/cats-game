b0545fce8949e74fba042c2d2961740f23f98f14
INF-png <infinite_2024@qq.com>
2026-08-18 09:45:53 +0800

    Add js/entity.js

---
+++ js/entity.js
@@ -0,0 +1,20 @@
+// entity.js - 实体基类
+export class Entity {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.angle = 0;
+    this.active = true;
+  }
+
+  get left()   { return this.x - this.width / 2; }
+  get right()  { return this.x + this.width / 2; }
+  get top()    { return this.y - this.height / 2; }
+  get bottom() { return this.y + this.height / 2; }
+
+  destroy() {
+    this.active = false;
+  }
+}
